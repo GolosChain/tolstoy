8740e73fb00e25634c23d0480003fb97dc478c76
dmitryorelopt <dmitryorelopt@gmail.com>
2018-09-04 13:27:57 +0300

    added post selector

--- app/components/pages/Post.jsx
+++ app/components/pages/Post.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import Comment from 'app/components/cards/Comment';
 import PostFull from 'app/components/cards/PostFull';
-import {connect} from 'react-redux';
-import {sortComments} from 'app/utils/comments';
+import { connect } from 'react-redux';
+import { sortComments } from 'app/utils/comments';
 import FoundationDropdownMenu from 'app/components/elements/FoundationDropdownMenu';
 import IllegalContentMessage from 'app/components/elements/IllegalContentMessage';
-import {Set} from 'immutable'
+import { Set } from 'immutable';
 import tt from 'counterpart';
 import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
 import { blockedUsers } from 'app/utils/IllegalContent';
 import { buttonClick } from 'app/utils/Analytics';
 
-import CTABlock from '../elements/CTA/CTABlock'
+import CTABlock from '../elements/CTA/CTABlock';
 
 class Post extends React.Component {
-
     static propTypes = {
         content: PropTypes.object.isRequired,
         post: PropTypes.string,
@@ -27,9 +26,9 @@ class Post extends React.Component {
     constructor() {
         super();
         this.state = {
-            showNegativeComments: false
+            showNegativeComments: false,
         };
-        this.shouldComponentUpdate = shouldComponentUpdate(this, 'Post')
+        this.shouldComponentUpdate = shouldComponentUpdate(this, 'Post');
     }
 
     componentDidMount() {
@@ -39,26 +38,27 @@ class Post extends React.Component {
         }
     }
 
-    toggleNegativeReplies = (e) => {
+    toggleNegativeReplies = e => {
         this.setState({
-            showNegativeComments: !this.state.showNegativeComments
+            showNegativeComments: !this.state.showNegativeComments,
         });
         e.preventDefault();
     };
 
     onHideComment = () => {
-        this.setState({commentHidden: true})
-    }
+        this.setState({ commentHidden: true });
+    };
 
     showAnywayClick = () => {
-        this.setState({showAnyway: true})
-    }
+        this.setState({ showAnyway: true });
+    };
 
     render() {
-        const LIQUID_TOKEN = tt('token_names.LIQUID_TOKEN')
+        console.log(this.props);
+        const LIQUID_TOKEN = tt('token_names.LIQUID_TOKEN');
 
-        const {current_user, ignoring, content} = this.props
-        const {showNegativeComments, commentHidden, showAnyway} = this.state
+        const { current_user, ignoring, content } = this.props;
+        const { showNegativeComments, commentHidden, showAnyway } = this.state;
         let post = this.props.post;
         const aiPosts = this.props.aiPosts;
         if (!post) {
@@ -69,30 +69,40 @@ class Post extends React.Component {
 
         if (!dis) return null;
 
-        if(!showAnyway) {
-            const {gray} = dis.get('stats').toJS()
-            if(gray) {
+        if (!showAnyway) {
+            const { gray } = dis.get('stats').toJS();
+            if (gray) {
                 return (
                     <div className="Post">
                         <div className="row">
                             <div className="column">
                                 <div className="PostFull">
-                                    <p onClick={this.showAnywayClick}>{tt('promote_post_jsx.this_post_was_hidden_due_to_low_ratings')}.{' '}
-                                    <button style={{marginBottom: 0}} className="button hollow tiny float-right" onClick={this.showAnywayClick}>{tt('g.show')}</button></p>
+                                    <p onClick={this.showAnywayClick}>
+                                        {tt(
+                                            'promote_post_jsx.this_post_was_hidden_due_to_low_ratings'
+                                        )}
+                                        .{' '}
+                                        <button
+                                            style={{ marginBottom: 0 }}
+                                            className="button hollow tiny float-right"
+                                            onClick={this.showAnywayClick}
+                                        >
+                                            {tt('g.show')}
+                                        </button>
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                        
                     </div>
-                )
+                );
             }
         }
 
         let replies = dis.get('replies').toJS();
 
         let sort_order = 'trending';
-        if( this.props.location && this.props.location.query.sort )
-           sort_order = this.props.location.query.sort;
+        if (this.props.location && this.props.location.query.sort)
+            sort_order = this.props.location.query.sort;
 
         const commentLimit = 100;
         if (global['process'] !== undefined && replies.length > commentLimit) {
@@ -100,18 +110,19 @@ class Post extends React.Component {
             replies = replies.slice(0, commentLimit);
         }
 
-        sortComments( content, replies, sort_order );
+        sortComments(content, replies, sort_order);
         const keep = a => {
             const c = content.get(a);
-            const hide = c.getIn(['stats', 'hide'])
-            let ignore = false
-            if(ignoring) {
-                ignore = ignoring.has(c.get('author'))
+            const hide = c.getIn(['stats', 'hide']);
+            let ignore = false;
+            if (ignoring) {
+                ignore = ignoring.has(c.get('author'));
                 // if(ignore) console.log(current_user && current_user.get('username'), 'is ignoring post author', c.get('author'), '\t', a)
             }
-            return !hide && !ignore
-        }
-        const positiveComments = replies.filter(a => keep(a))
+            return !hide && !ignore;
+        };
+        const positiveComments = replies
+            .filter(a => keep(a))
             .map(reply => (
                 <Comment
                     root
@@ -121,58 +132,82 @@ class Post extends React.Component {
                     sortOrder={sort_order}
                     showNegativeComments={showNegativeComments}
                     onHide={this.onHideComment}
-                />)
-            );
+                />
+            ));
 
-        const negativeGroup = commentHidden &&
-            (<div className="hentry Comment root Comment__negative_group">
+        const negativeGroup = commentHidden && (
+            <div className="hentry Comment root Comment__negative_group">
                 <p>
-                    {tt(showNegativeComments ? 'post_jsx.now_showing_comments_with_low_ratings' : 'post_jsx.comments_were_hidden_due_to_low_ratings')}.{' '}
-                    <button className="button hollow tiny float-right" onClick={e => this.toggleNegativeReplies(e)}>
-                        {tt(showNegativeComments ? 'g.hide' :'g.show')}
+                    {tt(
+                        showNegativeComments
+                            ? 'post_jsx.now_showing_comments_with_low_ratings'
+                            : 'post_jsx.comments_were_hidden_due_to_low_ratings'
+                    )}
+                    .{' '}
+                    <button
+                        className="button hollow tiny float-right"
+                        onClick={e => this.toggleNegativeReplies(e)}
+                    >
+                        {tt(showNegativeComments ? 'g.hide' : 'g.show')}
                     </button>
                 </p>
-            </div>);
+            </div>
+        );
 
-
-        let sort_orders = [ 'trending', 'votes', 'new', 'old'];
-        let sort_labels = [ tt('main_menu.trending'), tt('g.votes'), tt('g.created'), tt('g.old') ];
+        let sort_orders = ['trending', 'votes', 'new', 'old'];
+        let sort_labels = [tt('main_menu.trending'), tt('g.votes'), tt('g.created'), tt('g.old')];
         let sort_menu = [];
         let sort_label;
 
         let selflink = `/${dis.get('category')}/@${post}`;
-        for( let o = 0; o < sort_orders.length; ++o ){
-            if(sort_orders[o] == sort_order) sort_label = sort_labels[o];
+        for (let o = 0; o < sort_orders.length; ++o) {
+            if (sort_orders[o] == sort_order) sort_label = sort_labels[o];
             sort_menu.push({
                 value: sort_orders[o],
                 label: sort_labels[o],
-                link: selflink + '?sort=' + sort_orders[o] + '#comments'
+                link: selflink + '?sort=' + sort_orders[o] + '#comments',
             });
         }
-        const emptyPost = dis.get('created') === '1970-01-01T00:00:00' && dis.get('body') === ''
-        if(emptyPost)
-            return <center>
-                <div className="NotFound float-center">
-                    <div>
-                        <h4 className="NotFound__header">Sorry! This page doesnt exist.</h4>
-                        <p>Not to worry. You can head back to <a style={{fontWeight: 800}} href="/">our homepage</a>,
-                            or check out some great posts.
-                        </p>
-                        <ul className="NotFound__menu">
-                            <li><a href="/created">new posts</a></li>
-                            <li><a href="/hot">hot posts</a></li>
-                            <li><a href="/trending">trending posts</a></li>
-                            <li><a href="/promoted">promoted posts</a></li>
-                            <li><a href="/active">active posts</a></li>
-                        </ul>
+        const emptyPost = dis.get('created') === '1970-01-01T00:00:00' && dis.get('body') === '';
+        if (emptyPost)
+            return (
+                <center>
+                    <div className="NotFound float-center">
+                        <div>
+                            <h4 className="NotFound__header">Sorry! This page doesnt exist.</h4>
+                            <p>
+                                Not to worry. You can head back to{' '}
+                                <a style={{ fontWeight: 800 }} href="/">
+                                    our homepage
+                                </a>
+                                , or check out some great posts.
+                            </p>
+                            <ul className="NotFound__menu">
+                                <li>
+                                    <a href="/created">new posts</a>
+                                </li>
+                                <li>
+                                    <a href="/hot">hot posts</a>
+                                </li>
+                                <li>
+                                    <a href="/trending">trending posts</a>
+                                </li>
+                                <li>
+                                    <a href="/promoted">promoted posts</a>
+                                </li>
+                                <li>
+                                    <a href="/active">active posts</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </center>
+                </center>
+            );
 
-          if(blockedUsers.includes(post.split("/")[0])) {
-            return (<IllegalContentMessage />)
-          }
-          
+        if (blockedUsers.includes(post.split('/')[0])) {
+            return <IllegalContentMessage />;
+        }
+
         return (
             <div className="Post">
                 <div className="row">
@@ -180,54 +215,72 @@ class Post extends React.Component {
                         <PostFull post={post} cont={content} aiPosts={aiPosts} />
                     </div>
                 </div>
-                {!current_user && <div className="row">
-                <CTABlock post={post}/>
+                {!current_user && (
+                    <div className="row">
+                        <CTABlock post={post} />
 
-                    <div className="column">
-                        <div className="Post__promo">
-                            {tt('g.next_7_strings_sinngle_block.authors_get_paid_when_people_like_you_upvote_their_post')}.
-                            <br /> {tt('g.next_7_strings_sinngle_block.if_you_enjoyed_what_you_read_earn_amount')}
-                            <br />
-                            <a className="button sign-up" 
-                                href="/create_account"
-                                onClick={() => buttonClick()}
-                            >{tt('g.next_7_strings_sinngle_block.sign_up_now_to_receive')}
-                                <span className="free-money">{tt('g.next_7_strings_sinngle_block.free_steem', {LIQUID_TOKEN})}</span>
-                            </a>
+                        <div className="column">
+                            <div className="Post__promo">
+                                {tt(
+                                    'g.next_7_strings_sinngle_block.authors_get_paid_when_people_like_you_upvote_their_post'
+                                )}
+                                .<br />{' '}
+                                {tt(
+                                    'g.next_7_strings_sinngle_block.if_you_enjoyed_what_you_read_earn_amount'
+                                )}
+                                <br />
+                                <a
+                                    className="button sign-up"
+                                    href="/create_account"
+                                    onClick={() => buttonClick()}
+                                >
+                                    {tt('g.next_7_strings_sinngle_block.sign_up_now_to_receive')}
+                                    <span className="free-money">
+                                        {tt('g.next_7_strings_sinngle_block.free_steem', {
+                                            LIQUID_TOKEN,
+                                        })}
+                                    </span>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>}
+                )}
                 <div id="comments" className="Post_comments row hfeed">
                     <div className="column large-12">
                         <div className="Post_comments__content">
-                            {positiveComments.length ?
-                            (<div className="Post__comments_sort_order float-right">
-                                {tt('post_jsx.sort_order')}: &nbsp;
-                                <FoundationDropdownMenu menu={sort_menu} label={sort_label} dropdownPosition="bottom" dropdownAlignment="right" />
-                            </div>) : null}
+                            {positiveComments.length ? (
+                                <div className="Post__comments_sort_order float-right">
+                                    {tt('post_jsx.sort_order')}: &nbsp;
+                                    <FoundationDropdownMenu
+                                        menu={sort_menu}
+                                        label={sort_label}
+                                        dropdownPosition="bottom"
+                                        dropdownAlignment="right"
+                                    />
+                                </div>
+                            ) : null}
                             {positiveComments}
                             {negativeGroup}
                         </div>
                     </div>
-                </div>                
+                </div>
             </div>
         );
     }
 }
 
-const emptySet = Set()
+const emptySet = Set();
 
 export default connect(state => {
-    const current_user = state.user.get('current')
-    let ignoring
-    if(current_user) {
-        const key = ['follow', 'getFollowingAsync', current_user.get('username'), 'ignore_result']
-        ignoring = state.global.getIn(key, emptySet)
+    const current_user = state.user.get('current');
+    let ignoring;
+    if (current_user) {
+        const key = ['follow', 'getFollowingAsync', current_user.get('username'), 'ignore_result'];
+        ignoring = state.global.getIn(key, emptySet);
     }
     return {
         content: state.global.get('content'),
         current_user,
         ignoring,
-    }
-}
-)(Post);
+    };
+})(Post);
