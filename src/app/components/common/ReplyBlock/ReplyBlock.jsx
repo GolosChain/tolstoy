c64c2038dee83d86e7ff245d968f2ea958ea8419
dmitryorelopt <dmitryorelopt@gmail.com>
2018-09-10 12:53:15 +0300

    set default name

--- src/app/components/common/ReplyBlock/ReplyBlock.jsx
+++ src/app/components/common/ReplyBlock/ReplyBlock.jsx
@@ -41,7 +41,7 @@ const ReplyLink = styled(Link)`
     letter-spacing: 0.8px;
     color: #393636 !important;
     cursor: pointer;
-    
+
     ${isNot('showText')`
         @media (min-width: 890px) and (max-width: 1200px), (max-width: 639px) {
             display: none;
