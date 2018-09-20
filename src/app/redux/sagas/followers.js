76fec1433af65ecdc42b99ebac74d94945d91624
dmitryorelopt <dmitryorelopt@gmail.com>
2018-09-20 13:46:45 +0300

    added saga loadUserFollow

---
+++ src/app/redux/sagas/followers.js
@@ -0,0 +1,11 @@
+import { call, takeEvery } from 'redux-saga/effects';
+import { USER_FOLLOW_DATA_LOAD } from '../constants/followers';
+import { fetchFollowCount } from 'app/redux/sagas/follow';
+
+export default function* watch() {
+    yield takeEvery(USER_FOLLOW_DATA_LOAD, loadUserFollowData);
+}
+
+function* loadUserFollowData({ payload }) {
+    yield call(fetchFollowCount, payload.username);
+}
