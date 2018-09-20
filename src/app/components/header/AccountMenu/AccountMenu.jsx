9edd549acc59dc366eb63bf5b1bd00b4e514185b
Ilya Lebedev <bacherko@gmail.com>
2018-09-20 12:30:46 +0300

    fix account menu icons #832

--- src/app/components/header/AccountMenu/AccountMenu.jsx
+++ src/app/components/header/AccountMenu/AccountMenu.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import styled from 'styled-components';
 import tt from 'counterpart';
-import Icon from 'app/components/elements/Icon';
+import Icon from 'golos-ui/Icon';
 import user from 'app/redux/User';
 import { getAccountPrice } from 'src/app/redux/selectors/account/accountPrice';
 import { formatCurrency } from 'src/app/helpers/currency';
@@ -34,7 +34,7 @@ const Li = styled.li`
 const IconWrapper = styled.div`
     display: flex;
     justify-content: center;
-    width: 24px;
+    width: 26px;
     margin-right: 20px;
     overflow: hidden;
 `;
@@ -83,28 +83,39 @@ export default class AccountMenu extends PureComponent {
         let items = [
             {
                 link: `/@${myAccountName}/feed`,
-                icon: 'new/home',
-                iconSize: '1_5x',
+                icon: 'home',
                 text: tt('g.feed'),
+                size: 26,
             },
-            { link: `/@${myAccountName}`, icon: 'new/blogging', text: tt('g.blog') },
-            { link: `/@${myAccountName}/comments`, icon: 'new/comment', text: tt('g.comments') },
-            $STM_Config.is_sandbox
-                ? {
-                      icon: 'chatboxes',
-                      text: tt('g.messages'),
-                      onClick: onShowMessagesClick,
-                  }
-                : null,
+            { link: `/@${myAccountName}`, icon: 'blog', text: tt('g.blog'), size: 20 },
+            {
+                link: `/@${myAccountName}/comments`,
+                icon: 'comment',
+                text: tt('g.comments'),
+                size: 22,
+            },
             {
                 link: `/@${myAccountName}/recent-replies`,
-                icon: 'new/answer',
+                icon: 'comment-reply',
                 text: tt('g.replies'),
             },
-            { link: `/@${myAccountName}/favorites`, icon: 'new/star', text: tt('g.favorites') },
-            { link: `/@${myAccountName}/transfers`, icon: 'new/wallet', text: tt('g.wallet') },
-            { link: `/@${myAccountName}/settings`, icon: 'new/setting', text: tt('g.settings') },
-            { icon: 'new/logout', text: tt('g.logout'), onClick: onLogoutClick },
+            $STM_Config.is_sandbox
+                ? {
+                      icon: 'messanger',
+                      text: tt('g.messages'),
+                      onClick: onShowMessagesClick,
+                      size: 22,
+                  }
+                : null,
+            { link: `/@${myAccountName}/favorites`, icon: 'star', text: tt('g.favorites') },
+            {
+                link: `/@${myAccountName}/transfers`,
+                icon: 'wallet2',
+                text: tt('g.wallet'),
+                size: 22,
+            },
+            { link: `/@${myAccountName}/settings`, icon: 'settings', text: tt('g.settings') },
+            { icon: 'logout', text: tt('g.logout'), onClick: onLogoutClick, size: 22 },
         ];
 
         items = items.filter(item => item);
@@ -119,11 +130,11 @@ export default class AccountMenu extends PureComponent {
                     </div>
                 </PriceBlock>
                 <Ul>
-                    {items.map(({ link, icon, iconSize, text, onClick }, i) => (
+                    {items.map(({ link, icon, text, size, onClick }, i) => (
                         <Li key={i}>
                             <LinkStyled href={link} onClick={onClick}>
                                 <IconWrapper>
-                                    <IconStyled name={icon} size={iconSize || '1_25x'} />
+                                    <IconStyled name={icon} size={size || 24} />
                                 </IconWrapper>
                                 {text}
                             </LinkStyled>
