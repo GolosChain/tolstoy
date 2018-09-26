3471bb6f4eb25b9d921e050e0938abc9117f1e0a
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-09-26 13:17:00 +0300

    refactoring share in social networks functional

--- src/app/containers/post/SharePopover.jsx
+++ src/app/containers/post/SharePopover.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import is from 'styled-is';
+import tt from 'counterpart';
 
 import Icon from 'golos-ui/Icon';
-
-import { sharePopoverSelector } from 'src/app/redux/selectors/post/sharePopoverSelector';
+import { fbShare, ljShare, twitterShare, vkShare } from 'src/app/helpers/shareSocNetworks';
 
 const Wrapper = styled.div`
     ${is('horizontal')`
@@ -23,7 +22,6 @@ const ItemContainer = styled.div`
     }
 `;
 
-@connect(sharePopoverSelector)
 export default class SharePopover extends Component {
     static propTypes = {
         horizontal: PropTypes.bool,
@@ -34,7 +32,37 @@ export default class SharePopover extends Component {
     };
 
     render() {
-        const { shareMenu, horizontal } = this.props;
+        const { horizontal } = this.props;
+        const shareMenu = [
+            {
+                link: '#',
+                onClick: e => ljShare(e, post),
+                value: 'LJ',
+                title: tt('postfull_jsx.share_on_lj'),
+                icon: 'lj',
+            },
+            {
+                link: '#',
+                onClick: e => vkShare(e, post),
+                value: 'VK',
+                title: tt('postfull_jsx.share_on_vk'),
+                icon: 'vk',
+            },
+            {
+                link: '#',
+                onClick: e => fbShare(e, post),
+                value: 'Facebook',
+                title: tt('postfull_jsx.share_on_facebook'),
+                icon: 'facebook',
+            },
+            {
+                link: '#',
+                onClick: e => twitterShare(e, post),
+                value: 'Twitter',
+                title: tt('postfull_jsx.share_on_twitter'),
+                icon: 'twitter',
+            },
+        ];
         return (
             <Wrapper horizontal={horizontal}>
                 {shareMenu.map(item => (
