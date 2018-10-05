afeccad2da07c2bd780e41a55753dbfdce9e5449
dmitryorelopt <dmitryorelopt@gmail.com>
2018-10-05 13:13:11 +0300

    refactored side panel

---
+++ src/app/containers/post/sidePanel/SidePanel.connect.jsx
@@ -0,0 +1,35 @@
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
+import { currentUsernameSelector } from 'src/app/redux/selectors/common';
+import { toggleFavoriteAction } from 'src/app/redux/actions/favorites';
+import { onVote } from 'src/app/redux/actions/vote';
+import { reblog } from 'src/app/redux/actions/posts';
+import { SidePanel } from 'src/app/containers/post/sidePanel/SidePanel';
+import {
+    currentPostSelector,
+    authorSelector,
+    votesSummarySelector,
+} from 'src/app/redux/selectors/post/commonPost';
+
+export default connect(
+    createSelector(
+        [currentPostSelector, authorSelector, currentUsernameSelector, votesSummarySelector],
+        (post, author, username, votesSummary) => {
+            return {
+                votesSummary,
+                username,
+                isFavorite: post.isFavorite,
+                author: author.account,
+                permLink: post.permLink,
+                myVote: post.myVote,
+            };
+        }
+    ),
+
+    {
+        toggleFavoriteAction,
+        onVote,
+        reblog,
+    }
+)(SidePanel);
