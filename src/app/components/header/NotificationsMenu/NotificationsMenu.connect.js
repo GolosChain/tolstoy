09bcb5fb6a08be45e782966b344bf200c0223543
Alexey Elizarov <felex-ae@ya.ru>
2018-10-08 19:05:56 +0300

    refactoring notification menu connect #864

--- src/app/components/header/NotificationsMenu/NotificationsMenu.connect.js
+++ src/app/components/header/NotificationsMenu/NotificationsMenu.connect.js
@@ -1,7 +1,32 @@
 import { connect } from 'react-redux';
 
-import { notificationsMenuSelector } from 'src/app/redux/selectors/header/activity';
+import {
+    createDeepEqualSelector,
+    entitiesArraySelector,
+    statusSelector,
+} from 'src/app/redux/selectors/common';
+import { hydratedNotificationsSelector } from 'src/app/redux/selectors/notifications';
 
 import NotificationsMenu from './NotificationsMenu';
 
-export default connect(notificationsMenuSelector)(NotificationsMenu)
+const filteredNotificationsSelector = createDeepEqualSelector(
+    [entitiesArraySelector('notificationsOnline')],
+    notifications =>
+        notifications
+            .sortBy(a => a.get('createdAt'))
+            .reverse()
+            .take(5)
+);
+
+export default connect(
+    createDeepEqualSelector(
+        [
+            hydratedNotificationsSelector(filteredNotificationsSelector),
+            statusSelector('notificationsOnline'),
+        ],
+        (notifications, notificationsStatus) => ({
+            notifications,
+            isFetching: notificationsStatus.get('isFetching'),
+        })
+    )
+)(NotificationsMenu);
