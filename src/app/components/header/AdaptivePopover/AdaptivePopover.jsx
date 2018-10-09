e9adb43aa64f6e7b0eee7e70393891eb27d00a04
Alexey Elizarov <felex-ae@ya.ru>
2018-10-09 16:59:49 +0300

    delete AdaprivePopover and change its code #864

--- src/app/components/header/AdaptivePopover/AdaptivePopover.jsx
+++ src/app/components/header/AdaptivePopover/AdaptivePopover.jsx
@@ -1,24 +1,11 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import Popover from '../Popover';
+import MobilePopover from '../MobilePopover';
 
-import MobilePopover from './../MobilePopover';
-import Popover from './../Popover';
-
-export default class AdaptivePopover extends PureComponent {
-    static propTypes = {
-        isMobile: PropTypes.bool,
-        target: PropTypes.object.isRequired,
-        onClose: PropTypes.func.isRequired,
-        children: PropTypes.element.isRequired,
-    }
-
-    render() {
-        const { isMobile, target, onClose, children } = this.props;
-
-        if (isMobile) {
-            return <MobilePopover target={target} onClose={onClose}>{children}</MobilePopover>;
-        } else {
-            return <Popover target={target} onClose={onClose}>{children}</Popover>;
-        }
+export default function AdaptivePopover(props) {
+    if (props.isMobile) {
+        return <MobilePopover {...props} />;
+    } else {
+        return <Popover {...props} />;
     }
 }
