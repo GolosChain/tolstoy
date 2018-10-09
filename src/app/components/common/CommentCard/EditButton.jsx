ed01efa6a7b834f88fc175ca786eeeaeed5b048c
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-10-09 16:03:17 +0300

    edit button in single component start

---
+++ src/app/components/common/CommentCard/EditButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const EditButton = ({}) => {
+    <IconEditWrapper
+        data-tooltip={'Редактировать комментарий'}
+        onClick={this._onEditClick}
+    >
+        <Icon name="pen" size={20} />
+    </IconEditWrapper>
+};
