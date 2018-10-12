a7bff96969591299662c9127ea022a8eb56ed299
Ilya Lebedev <bacherko@gmail.com>
2018-10-12 18:10:14 +0300

    add settings check into CurrencyValue #914

--- src/app/components/common/CurrencyValue/CurrencyValue.connect.js
+++ src/app/components/common/CurrencyValue/CurrencyValue.connect.js
@@ -3,5 +3,6 @@ import { dataSelector } from 'src/app/redux/selectors/common';
 import CurrencyValue from './CurrencyValue';
 
 export default connect(state => ({
+    settings: dataSelector('settings')(state),
     rates: dataSelector('rates')(state),
 }))(CurrencyValue);
