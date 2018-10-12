1d6841a18bb78ed922bfd81f08abca78827eb5f4
Ilya Lebedev <bacherko@gmail.com>
2018-10-12 17:38:40 +0300

    fix 2 #914

--- src/app/components/common/CurrencyValue/CurrencyValue.jsx
+++ src/app/components/common/CurrencyValue/CurrencyValue.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { CURRENCIES } from 'app/client_config';
 import { renderValue } from 'src/app/helpers/currency';
 
-export default function CurrencyValue({
-    value,
-    currency,
-    toCurrency,
-    date,
-    rates,
-    className,
-    style,
-}) {
-    return (
-        <span className={className} style={style}>
-            {renderValue(value, currency, {
-                toCurrency,
-                date,
-                rates,
-            })}
-        </span>
-    );
+export default function CurrencyValue({ value, currency, className, ...props }) {
+    return <span className={className}>{renderValue(value, currency, props)}</span>;
 }
+
+CurrencyValue.propTypes = {
+    value: PropTypes.number.isRequired,
+    currency: PropTypes.oneOf(CURRENCIES).isRequired,
+    className: PropTypes.string,
+    decimals: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf(['short', 'adaptive'])]),
+    date: PropTypes.string,
+    toCurrency: PropTypes.oneOf(CURRENCIES),
+    rates: PropTypes.object,
+    settings: PropTypes.object,
+};
