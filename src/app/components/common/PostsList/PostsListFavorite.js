6b7472a4ad8ccf7974527f1dccb8f4a1efa118cf
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-10-18 13:44:31 +0300

    favorites posts reverse for see last added on the top

--- src/app/components/common/PostsList/PostsListFavorite.js
+++ src/app/components/common/PostsList/PostsListFavorite.js
@@ -1,21 +1,23 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
 import PostsList from './PostsList';
 import { favoritesLoadNextPageAction } from 'src/app/redux/actions/favorites';
 import { fetchCurrentStateAction } from 'src/app/redux/actions/fetch';
 
 export default connect(
-    state => {
+    createSelector([state => state], state => {
         const { isLoading, isPageLoading, showList } = state.data.favorites;
 
-        const layout = state.ui.profile && state.ui.profile.get('layout') || 'list';
+        const layout = (state.ui.profile && state.ui.profile.get('layout')) || 'list';
 
         return {
             isFavorite: true,
             layout,
             isLoading: isLoading || isPageLoading,
-            posts: showList,
+            posts: showList.reverse(),
         };
-    },
+    }),
     dispatch => ({
         loadMore() {
             dispatch(favoritesLoadNextPageAction());
