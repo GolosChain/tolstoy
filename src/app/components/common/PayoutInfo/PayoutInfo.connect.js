d00325891679e2bcc0ba6ed9949e3115f3782008
orelDmitriy <dmitryorelopt@gmail.com>
2018-10-19 09:53:39 +0300

    formatted with prettier

--- src/app/components/common/PayoutInfo/PayoutInfo.connect.js
+++ src/app/components/common/PayoutInfo/PayoutInfo.connect.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 
-import {getPayoutPermLink} from 'src/app/redux/selectors/payout/common';
+import { getPayoutPermLink } from 'src/app/redux/selectors/payout/common';
 import { getHistoricalData } from 'src/app/redux/actions/rates';
 import PayoutInfo from './PayoutInfo';
 
