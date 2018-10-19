6221e4f532498943d84da9a0b1c25638ad56b1ff
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-10-19 16:34:03 +0300

    Header.connect json_metadata checking

--- src/app/components/header/Header/Header.connect.js
+++ src/app/components/header/Header/Header.connect.js
@@ -1,4 +1,7 @@
 import { connect } from 'react-redux';
+import { Map } from 'immutable';
+
+import normalizeProfile from 'app/utils/NormalizeProfile';
 
 import user from 'app/redux/User';
 import {
@@ -9,26 +12,31 @@ import { statusSelector } from 'src/app/redux/selectors/common';
 
 import Header from './Header';
 
+const emptyMap = Map();
+
 export default connect(
     state => {
-        const currentAccountName = state.user.getIn(['current', 'username']);
+        const currentUsername = state.user.getIn(['current', 'username']);
 
         let votingPower = null,
             realName = null;
 
-        if (currentAccountName) {
-            votingPower =
-                state.global.getIn(['accounts', currentAccountName, 'voting_power']) / 100;
-            realName =
-                JSON.parse(state.global.getIn(['accounts', currentAccountName, 'json_metadata']))
-                    .profile.name || currentAccountName;
+        if (currentUsername) {
+            votingPower = state.global.getIn(['accounts', currentUsername, 'voting_power']) / 100;
+
+            const userData = state.global.getIn(['accounts', currentUsername]) || emptyMap;
+            const jsonData = normalizeProfile({
+                json_metadata: userData.get('json_metadata'),
+                name: currentUsername,
+            });
+            realName = jsonData.name || currentUsername;
         }
 
         const notificationsOnlineStatus = statusSelector('notificationsOnline')(state);
 
         return {
             freshCount: notificationsOnlineStatus.get('freshCount'),
-            currentAccountName,
+            currentUsername,
             votingPower,
             realName,
             offchainAccount: state.offchain.get('account'),
