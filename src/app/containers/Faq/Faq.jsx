3946bb8559ca0121af82a90917ce5bd4bf5be120
b1acksun <tkachenko.igor88@gmail.com>
2018-10-23 20:22:43 +0300

    fix locale #967 #1093

--- src/app/containers/Faq/Faq.jsx
+++ src/app/containers/Faq/Faq.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import tt from 'counterpart';
+
 import Header from '../../components/faq/Header';
 import QuestionsList from '../../components/faq/QuestionsList';
 import Channels from '../../components/faq/Channels';
@@ -12,11 +14,8 @@ const Wrapper = styled.div`
 export default class Faq extends Component {
     constructor() {
         super();
-        let lang;
-        if (process.env.BROWSER) {
-            lang = localStorage.getItem('language');
-        }
-
+        const lang = tt.getLocale();
+        
         this.questions = require(lang === 'en'
             ? './questions_EN.json'
             : './questions_RU.json');
