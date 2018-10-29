ed509f57fd1bc316830d3c188ea71048ab79919a
Alexey Elizarov <felex-ae@ya.ru>
2018-10-29 13:31:04 +0300

    refactoring FollowersDialog and follow button, collapse option for follow button #1238

---
+++ src/app/components/dialogs/FollowersDialog/FollowersDialog.connect.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+
+import { followersDialogSelector } from 'src/app/redux/selectors/dialogs/followersDialog';
+import { getFollowers, getFollowing } from 'src/app/redux/actions/followers';
+
+import FollowersDialog from './FollowersDialog';
+
+export default connect(
+    followersDialogSelector,
+    {
+        getFollowers,
+        getFollowing,
+    }
+)(FollowersDialog);
