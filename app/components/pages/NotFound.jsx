ca039e667ce91049172324a868c702fe4e066514
Ilya Lebedev <bacherko@gmail.com>
2018-10-31 17:23:24 +0300

    remove beta from NotFound page #1317

--- app/components/pages/NotFound.jsx
+++ app/components/pages/NotFound.jsx
@@ -4,7 +4,6 @@ import Icon from 'app/components/elements/Icon.jsx';
 import { APP_NAME, APP_ICON } from 'app/client_config';
 
 class NotFound extends React.Component {
-
     render() {
         return (
             <div>
@@ -13,11 +12,13 @@ class NotFound extends React.Component {
                         <div className="top-bar-left">
                             <ul className="menu" style={{ alignItems: 'center' }}>
                                 <li className="Header__top-logo">
-                                    <Link to='/'>
+                                    <Link to="/">
                                         <Icon name={APP_ICON} size="2x" />
                                     </Link>
                                 </li>
-                                <li className="Header__top-steemit show-for-medium noPrint"><a href="/">{APP_NAME}<span className="beta">beta</span></a></li>
+                                <li className="Header__top-steemit show-for-medium noPrint">
+                                    <a href="/">{APP_NAME}</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -26,15 +27,29 @@ class NotFound extends React.Component {
                     <div>
                         <Icon name={APP_ICON} size="4x" />
                         <h4 className="NotFound__header">Sorry! This page doesn't exist.</h4>
-                        <p>Not to worry. You can head back to <a style={{fontWeight: 800}} href="/">our homepage</a>,
-                           or check out some great posts.
+                        <p>
+                            Not to worry. You can head back to{' '}
+                            <a style={{ fontWeight: 800 }} href="/">
+                                our homepage
+                            </a>
+                            , or check out some great posts.
                         </p>
                         <ul className="NotFound__menu">
-                          <li><a href="/created">new posts</a></li>
-                          <li><a href="/hot">hot posts</a></li>
-                          <li><a href="/trending">trending posts</a></li>
-                          <li><a href="/promoted">promoted posts</a></li>
-                          <li><a href="/active">active posts</a></li>
+                            <li>
+                                <a href="/created">new posts</a>
+                            </li>
+                            <li>
+                                <a href="/hot">hot posts</a>
+                            </li>
+                            <li>
+                                <a href="/trending">trending posts</a>
+                            </li>
+                            <li>
+                                <a href="/promoted">promoted posts</a>
+                            </li>
+                            <li>
+                                <a href="/active">active posts</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -45,5 +60,5 @@ class NotFound extends React.Component {
 
 export default {
     path: '*',
-    component: NotFound
+    component: NotFound,
 };
