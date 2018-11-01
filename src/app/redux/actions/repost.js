fce764eda5c0385cb2dafa5f413dc19966763f75
Ilya Lebedev <bacherko@gmail.com>
2018-11-01 12:52:04 +0300

    fix empty repost #1347

--- src/app/redux/actions/repost.js
+++ src/app/redux/actions/repost.js
@@ -1,27 +1,27 @@
 import transaction from 'app/redux/Transaction';
 
 export const repost = ({ myAccountName, author, permLink, comment, onSuccess, onError }) => {
-    const json = [
-        'reblog',
-        {
-            account: myAccountName,
-            author,
-            permlink: permLink,
-            title: '',
-            body: comment,
-            json_metadata: JSON.stringify({
-                app: 'golos.io/0.1',
-                format: 'text',
-            }),
-        },
-    ];
+    const operationJson = {
+        account: myAccountName,
+        author,
+        permlink: permLink,
+    };
+
+    if (comment) {
+        operationJson.title = '';
+        operationJson.body = comment;
+        operationJson.json_metadata = JSON.stringify({
+            app: 'golos.io/0.1',
+            format: 'text',
+        });
+    }
 
     return transaction.actions.broadcastOperation({
         type: 'custom_json',
         operation: {
             id: 'follow',
             required_posting_auths: [myAccountName],
-            json: JSON.stringify(json),
+            json: JSON.stringify(['reblog', operationJson]),
         },
         successCallback: onSuccess,
         errorCallback: onError,
