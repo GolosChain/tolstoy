49c79347dc69a9073568ff7a240bdf2d5305b094
Ilya Lebedev <bacherko@gmail.com>
2018-11-19 14:13:00 +0300

    1588 fix layout gaps (#1593)
    * fix layout gaps #1588
    
    * extract base Navigation component #1588
    
    * fix active index link #1588
    
    * rename constants #1588
    
    * review fixes #1588

--- src/app/components/common/Container/Container.jsx
+++ src/app/components/common/Container/Container.jsx
@@ -2,20 +2,24 @@ import styled from 'styled-components';
 import is from 'styled-is';
 import Flex from 'golos-ui/Flex';
 
-export const MAX_WIDTH = 1150;
-export const OFFSET = 20;
+export const CONTAINER_MAX_WIDTH = 1150;
+export const CONTAINER_MOBILE_WIDTH = 500;
+export const CONTAINER_BASE_MARGIN = 20;
+export const CONTAINER_FULL_WIDTH = CONTAINER_MAX_WIDTH + 2 * CONTAINER_BASE_MARGIN;
+export const CONTAINER_MOBILE_MARGIN = 16;
 
 const Container = styled(Flex)`
-    max-width: ${MAX_WIDTH}px;
+    max-width: ${CONTAINER_MAX_WIDTH}px;
     margin: 0 auto;
 
-    @media (max-width: ${MAX_WIDTH + OFFSET * 2}px) {
-        margin: 0 ${OFFSET}px;
+    @media (max-width: ${CONTAINER_FULL_WIDTH}px) {
+        margin-left: ${CONTAINER_BASE_MARGIN}px;
+        margin-right: ${CONTAINER_BASE_MARGIN}px;
+    }
 
-        ${is('usePadding')`
-            margin: 0;
-            padding: 0 ${OFFSET}px;
-        `};
+    @media (max-width: ${CONTAINER_MOBILE_WIDTH}px) {
+        margin-left: ${CONTAINER_MOBILE_MARGIN}px;
+        margin-right: ${CONTAINER_MOBILE_MARGIN}px;
     }
 
     ${is('small')`
---
+++ src/app/components/main/MainNavigation/MainNavigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import tt from 'counterpart';
+
+import { TabLink } from 'golos-ui/Tab';
+import Navigation from 'src/app/components/common/Navigation';
+import LayoutSwitcher from 'src/app/components/common/LayoutSwitcher';
+
+export default function MainNavigation({ myAccountName, className }) {
+    const tabLinks = [];
+
+    if (myAccountName) {
+        tabLinks.push({ value: tt('header_jsx.home'), to: `/@${myAccountName}/feed` });
+    }
+
+    tabLinks.push(
+        { value: tt('g.new'), to: '/created' },
+        { value: tt('main_menu.hot'), to: '/hot' },
+        { value: tt('main_menu.trending'), to: '/trending', index: true },
+        { value: tt('g.promoted'), to: '/promoted' }
+    );
+
+    return <Navigation tabLinks={tabLinks} rightItems={<LayoutSwitcher />} className={className} />;
+}
