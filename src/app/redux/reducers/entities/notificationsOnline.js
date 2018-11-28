278390b1ce758eb4b0455672ced71e07b4ed02af
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-11-28 18:16:45 +0300

    fix cleaning notification online field

--- src/app/redux/reducers/entities/notificationsOnline.js
+++ src/app/redux/reducers/entities/notificationsOnline.js
@@ -1,6 +1,7 @@
 import { Map } from 'immutable';
 
 import { NOTIFICATION_ONLINE_GET_HISTORY } from 'src/app/redux/constants/notificationsOnline';
+import { NOTIFICATION_MARK_ALL_AS_VIEWED_SUCCESS } from 'src/app/redux/constants/notifications';
 
 const initialState = Map();
 
@@ -8,6 +9,9 @@ const initialState = Map();
 export default function(state = initialState, { type, payload }) {
     switch (type) {
         case NOTIFICATION_ONLINE_GET_HISTORY:
+            return initialState;
+
+        case NOTIFICATION_MARK_ALL_AS_VIEWED_SUCCESS:
             return initialState;
 
         default:
