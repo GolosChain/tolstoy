6e339fc13f51324b93354e3aa3f0dd0fef4d6b4d
Nikita-Pobazhak <nikita.pobazhak@gmail.com>
2018-12-04 13:17:40 +0300

    mobile view of error page

--- src/app/containers/ContentErrorBoundary.jsx
+++ src/app/containers/ContentErrorBoundary.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link  } from 'react-router';
+import tt from 'counterpart';
 import styled from 'styled-components';
 
 import Icon from 'golos-ui/Icon';
@@ -17,6 +18,10 @@ const Wrapper = styled.div`
 const Content = styled.div`
     display: flex;
     flex-grow: 1;
+    
+    @media (max-width: 945px) {
+        flex-direction: column;
+    }
 `;
 
 const Info = styled.div`
@@ -25,6 +30,12 @@ const Info = styled.div`
     justify-content: center;
     flex-basis: 280px;
     margin-right: 60px;
+    
+    @media (max-width: 945px) {
+        align-items: center;
+        flex-basis: auto;
+        margin-right: 0;
+    }
 `;
 
 const InfoTitle = styled.div`
@@ -33,22 +44,39 @@ const InfoTitle = styled.div`
     font-weight: 900;
     line-height: 1.21;
     letter-spacing: 0.4px;
+    
+    @media (max-width: 945px) {
+        margin-bottom: 10px;
+    }
 `;
 
 const InfoText = styled.div`
     font-size: 16px;
     line-height: 1.38;
     letter-spacing: -0.3px;
+    
+    @media (max-width: 945px) {
+        text-align: center;
+    }
 `;
 
 const TryReloadText = styled(InfoText)`
     margin-top: 30px;
+    color: #959595;
+    
+    @media (max-width: 945px) {
+        margin-top: 10px;
+    }
 `;
 
 const Image = styled.div`
     flex-grow: 1;
     height: 435px;
     background: url('/images/errors/content-error-boundary-logo.svg') center no-repeat;
+    
+    @media (max-width: 945px) {
+        background-size: contain;
+    }
 `;
 
 const ReloadButton = styled.div`
@@ -70,6 +98,14 @@ const ReloadButton = styled.div`
     & ${Icon} {
         margin-right: 8px;
     }
+    
+    &:hover {
+        background: #0e69ff;
+    }
+    
+    @media (max-width: 945px) {
+        margin-top: 20px;
+    }
 `;
 
 const WrapperLink = styled(Link)`
@@ -80,10 +116,14 @@ const WrapperLink = styled(Link)`
 
 const Links = styled.div`
     display: flex;
-    margin-top: 75px;
+    margin-top: 65px;
     
     & ${WrapperLink}:first-child {
         margin-right: 30px;
+    }
+    
+    @media (max-width: 945px) {
+        margin-top: 20px;
     }
 `;
 
@@ -100,6 +140,10 @@ export default class ContentErrorBoundary extends Component {
         });
     }
 
+    reloadPage = () => {
+        window.location.reload();
+    };
+
     render() {
         if (this.state.hasError) {
             return (
@@ -112,13 +156,13 @@ export default class ContentErrorBoundary extends Component {
                                 работаем над её устранением. Попробуйте обновить страницу.
                             </InfoText>
                             <TryReloadText>Попробуйте обновить страницу.</TryReloadText>
-                            <ReloadButton>
+                            <ReloadButton onClick={this.reloadPage}>
                                 <Icon name="reload" size={14} />
                                 обновить
                             </ReloadButton>
                             <Links>
                                 <WrapperLink to="/">Перейти на главную</WrapperLink>
-                                <WrapperLink to="/" target="_blank">Написать в поддержку</WrapperLink>
+                                <WrapperLink to={supportLink()} target="_blank" rel="noopener norefferer">Написать в поддержку</WrapperLink>
                             </Links>
                         </Info>
                         <Image />
@@ -128,5 +172,17 @@ export default class ContentErrorBoundary extends Component {
         }
 
         return this.props.children;
+    }
+}
+
+function supportLink() {
+    const locale = tt.getLocale();
+
+    switch (locale) {
+        case 'en':
+        case 'uk':
+            return 'https://t.me/golos_eng';
+        case 'ru':
+            return 'https://t.me/golos_support';
     }
 }
