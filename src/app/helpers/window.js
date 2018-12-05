6f5d9b8de0e2ceb1872cb88cfb1a4e74ccc384e8
Alexey Rekish <alexeyrekish@gmail.com>
2018-12-05 12:49:04 +0300

    1607 scroll to added comment (#1720)
    * 1607 added scroll to added comment
    
    * fix for pr

--- src/app/helpers/window.js
+++ src/app/helpers/window.js
@@ -12,3 +12,31 @@ export function getScrollElement() {
 export function checkSmallScreen() {
     return window.innerWidth <= SMALL_SCREEN_WIDTH;
 }
+
+export const smoothScroll = (targetY, duration) => {
+    const startingY = window.pageYOffset;
+    const diff = targetY - startingY;
+
+    let start;
+
+    if (!diff) {
+        return;
+    }
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+        if (!start) {
+            start = timestamp;
+        }
+
+        const time = timestamp - start;
+        const percent = Math.min(time / duration, 1);
+
+        window.scrollTo(0, startingY + diff * percent);
+
+        if (time < duration) {
+            window.requestAnimationFrame(step);
+        }
+    }
+};
