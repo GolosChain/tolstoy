863d56d0e257a8d4b44af017f9b8ea58ff4a71f4
Alexey Rekish <alexeyrekish@gmail.com>
2018-12-12 16:27:20 +0300

    1510 add registration block to post (#1784)
    * start working
    
    * 1510 add registration block to post

--- src/app/containers/post/PostContainer.jsx
+++ src/app/containers/post/PostContainer.jsx
@@ -103,7 +103,7 @@ export class PostContainer extends Component {
     };
 
     render() {
-        const { postLoaded, newVisitor, isOwner, isHidden } = this.props;
+        const { postLoaded, user, isOwner, isHidden } = this.props;
         const { showAlert } = this.state;
         if (!postLoaded) {
             return <Loader type="circle" center size={40} />;
@@ -142,8 +142,8 @@ export class PostContainer extends Component {
                         toggleFavorite={this.toggleFavorite}
                         postContentRef={this.postContentRef}
                     />
-                    <CommentsContainer />
-                    {newVisitor && <RegistrationPanel />}
+                    {!user && <RegistrationPanel />}
+                    <CommentsContainer user={user} />
                 </ContentWrapper>
             </Wrapper>
         );
