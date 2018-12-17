4cc2dfc1c8fb86029f738cf1f2ae8d8fc7c0bfaa
Alexey Rekish <alexeyrekish@gmail.com>
2018-12-17 18:20:03 +0300

    1767 add user statuses (#1800)
    * added status to user info
    
    * added user status to user popover
    
    * almost done
    
    * 1767 add user statuses
    
    * remove console.log
    
    * change whale icon
    
    * fix pin position

--- src/app/containers/post/popoverBody/PopoverBody.connect.jsx
+++ src/app/containers/post/popoverBody/PopoverBody.connect.jsx
@@ -1,21 +1,36 @@
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 
-import { currentUsernameSelector } from 'src/app/redux/selectors/common';
+import { currentUsernameSelector, globalSelector } from 'src/app/redux/selectors/common';
 import { authorSelector } from 'src/app/redux/selectors/post/commonPost';
 import { USER_PINNED_POSTS_LOAD } from 'src/app/redux/constants/pinnedPosts';
 import { PopoverBody } from 'src/app/containers/post/popoverBody/PopoverBody';
+import { repLog10 } from 'app/utils/ParsersAndFormatters';
 
 export default connect(
-    createSelector([authorSelector, currentUsernameSelector], (author, currentUsername) => ({
-        account: author.account,
-        name: author.name,
-        about: author.about,
-        followerCount: author.followerCount,
-        pinnedPosts: author.pinnedPosts,
-        pinnedPostsUrls: author.pinnedPostsUrls,
-        showFollowBlock: author.account !== currentUsername,
-    })),
+    createSelector(
+        [
+            authorSelector,
+            currentUsernameSelector,
+            (state, props) => globalSelector(['accounts', props.author])(state),
+        ],
+        (author, currentUsername, user) => {
+            if (!user) {
+                return {};
+            }
+
+            return {
+                account: author.account,
+                name: author.name,
+                about: author.about,
+                followerCount: author.followerCount,
+                pinnedPosts: author.pinnedPosts,
+                pinnedPostsUrls: author.pinnedPostsUrls,
+                showFollowBlock: author.account !== currentUsername,
+                reputation: repLog10(user.get('reputation')),
+            };
+        }
+    ),
 
     {
         getPostContent: urls => ({
