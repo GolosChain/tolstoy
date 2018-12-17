0d129795597ca26f4cb3c712d3d3756cf074661a
Igor Tkachenko <tkachenko.igor88@gmail.com>
2018-12-17 17:01:43 +0200

    1251 change locale component styles (#1820)
    Компонент был взят отсюда: [LangSwitch](https://github.com/GolosChain/registration-frontend/blob/master/components/LangSwitch.js) и немного адаптирован

--- src/app/components/header/LocaleSelect/LocaleSelect.jsx
+++ src/app/components/header/LocaleSelect/LocaleSelect.jsx
@@ -1,46 +1,140 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
+import is from 'styled-is';
 
-import { Select } from 'golos-ui/Form';
 import { LANGUAGES } from 'app/client_config';
 
-const StyledSelect = styled(Select)`
-    font-weight: 500;
-    border: none;
+const Wrapper = styled.div`
+    position: relative;
+    margin-left: 1em;
     cursor: pointer;
+    z-index: 1;
+`;
 
-    &:focus {
-        border: none;
-    }
+const Current = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 60px;
+    height: 34px;
+    font-weight: 500;
+    text-transform: uppercase;
+    color: #393636;
+    user-select: none;
+    z-index: 1;
+`;
 
-    @media (max-width: 576px) {
-        padding: 9px 14px;
-        background-image: none;
-    }
+const Chevron = styled.div`
+    position: absolute;
+    top: 15px;
+    right: 8px;
+    border: 3px solid transparent;
+    border-top-color: #363636;
+    ${is('open')`
+        top: 11px;
+        border-top-color: transparent;
+        border-bottom-color: #363636;
+    `};
+`;
 
-    @media (max-width: 350px) {
-        margin-right: 4px;
-        padding: 9px 10px;
+const List = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    top: -4px;
+    left: 0;
+    right: 0;
+    padding: 38px 0 4px;
+    border-radius: 8px;
+    background: #fff;
+    border-color: #3684ff;
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+    opacity: 0;
+    transition: opacity 0.4s;
+    pointer-events: none;
+    ${is('open')`
+        opacity: 1;
+        pointer-events: initial;
+    `};
+`;
+
+const ListItem = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 34px;
+    font-weight: 500;
+    text-transform: uppercase;
+    color: #959595;
+    cursor: pointer;
+    user-select: none;
+    &:hover {
+        color: #333;
     }
 `;
 
-const LocaleSelect = ({ currentUser, locale, onChangeLocale, setSettingsLocale }) => (
-    <StyledSelect
-        defaultValue={locale}
-        onChange={e => {
-            const { value } = e.target;
-            if (currentUser) {
-                setSettingsLocale(value);
-            }
-            onChangeLocale(value);
-        }}
-    >
-        {Object.keys(LANGUAGES).map(key => (
-            <option key={key} value={key}>
-                {LANGUAGES[key].shortValue}
-            </option>
-        ))}
-    </StyledSelect>
-);
+export default class LocaleSelect extends PureComponent {
+    state = {
+        open: false,
+    };
 
-export default LocaleSelect;
+    componentWillUnmount() {
+        window.removeEventListener('click', this.onAwayClick);
+    }
+
+    onRef = el => {
+        this.root = el;
+    };
+
+    onOpenClick = () => {
+        this.toggle(!this.state.open);
+    };
+
+    onAwayClick = e => {
+        if (!this.root.contains(e.target)) {
+            this.toggle(false);
+        }
+    };
+
+    toggle(show) {
+        if (show) {
+            window.addEventListener('click', this.onAwayClick);
+        } else {
+            window.removeEventListener('click', this.onAwayClick);
+        }
+
+        this.setState({
+            open: show,
+        });
+    }
+
+    render() {
+        const { currentUser, locale } = this.props;
+        const { open } = this.state;
+
+        return (
+            <Wrapper innerRef={this.onRef}>
+                <Current onClick={this.onOpenClick}>
+                    {LANGUAGES[locale].shortValue}
+                    <Chevron open={open} />
+                </Current>
+                <List open={open}>
+                    {Object.keys(LANGUAGES).map(lang => (
+                        <ListItem
+                            key={lang}
+                            onClick={() => {
+                                if (currentUser) {
+                                    this.props.setSettingsLocale(lang);
+                                }
+                                this.props.onChangeLocale(lang);
+                            }}
+                        >
+                            {LANGUAGES[lang].shortValue}
+                        </ListItem>
+                    ))}
+                </List>
+            </Wrapper>
+        );
+    }
+}
