53a2be905edeb2586c9619dc93ad49624376562e
b1acksun <tkachenko.igor88@gmail.com>
2018-12-25 11:53:11 +0200

    fix pr

--- src/app/components/userProfile/settings/keys/ResetKey/ResetKey.connect.js
+++ src/app/components/userProfile/settings/keys/ResetKey/ResetKey.connect.js
@@ -7,7 +7,7 @@ import ResetKey from './ResetKey';
 
 export default connect(
     null,
-    dispatch => ({
+    {
         changePassword: ({
             accountName,
             password,
@@ -17,31 +17,27 @@ export default connect(
             errorCallback,
         }) => {
             const ph = role => PrivateKey.fromSeed(`${accountName}${role}${newWif}`).toWif();
-            dispatch(
-                transaction.actions.updateAuthorities({
-                    accountName,
-                    auths: [
-                        { authType: 'owner', oldAuth: password, newAuth: ph('owner', newWif) },
-                        {
-                            authType: 'active',
-                            oldAuth: password,
-                            newAuth: ph('active', newWif),
-                        },
-                        {
-                            authType: 'posting',
-                            oldAuth: password,
-                            newAuth: ph('posting', newWif),
-                        },
-                        { authType: 'memo', oldAuth: password, newAuth: ph('memo', newWif) },
-                    ],
-                    clearAccountAuths,
-                    onSuccess: successCallback,
-                    onError: errorCallback,
-                })
-            );
+            return transaction.actions.updateAuthorities({
+                accountName,
+                auths: [
+                    { authType: 'owner', oldAuth: password, newAuth: ph('owner', newWif) },
+                    {
+                        authType: 'active',
+                        oldAuth: password,
+                        newAuth: ph('active', newWif),
+                    },
+                    {
+                        authType: 'posting',
+                        oldAuth: password,
+                        newAuth: ph('posting', newWif),
+                    },
+                    { authType: 'memo', oldAuth: password, newAuth: ph('memo', newWif) },
+                ],
+                clearAccountAuths,
+                onSuccess: successCallback,
+                onError: errorCallback,
+            });
         },
-        notify: (message, dismiss = 3000) => {
-            dispatch(showNotification(message, 'settings', dismiss));
-        },
-    })
+        notify: (message, dismiss = 3000) => showNotification(message, 'settings', dismiss),
+    }
 )(ResetKey);
