7fc7e35b1c809782bd6e69b2fb2bbfff6686642b
Alexey Rekish <alexeyrekish@gmail.com>
2018-12-25 13:22:02 +0300

    1850 l10n fix (#1869)
    * changed PostForm component
    
    * refactor EditorSwitcher
    
    * working with PostFooter
    
    * adapted buttons
    
    * tags done
    
    * post footer done
    
    * adapted creating the post
    
    * toolbar changes
    
    * comments done, editMode almost done
    
    * 1211 adapt text editor
    
    * 1211 adapt text editor
    
    * fixes for pr
    
    * vertical scroll fix
    
    * fix vertical overflow
    
    * fix duplication
    
    * fix hints visibility for webkit browsers
    
    * fix error hint visibility
    
    * 1850 post creating l10n error fix
    
    * fix for pr
    
    * fix buttons wrapper visibility

--- app/components/elements/postEditor/PostFooter/PostFooter.jsx
+++ app/components/elements/postEditor/PostFooter/PostFooter.jsx
@@ -208,7 +208,10 @@ export default class PostFooter extends PureComponent {
                             ) : (
                                 <Button onClick={this.props.onResetClick}>{tt('g.clear')}</Button>
                             )}
-                            <ConfirmButtonWrapper>
+                            <ConfirmButtonWrapper
+                                onMouseOver={this.showHint}
+                                onMouseOut={this.hideHint}
+                            >
                                 {postDisabled && disabledHint ? (
                                     <DisabledHint
                                         key="1"
@@ -227,8 +230,6 @@ export default class PostFooter extends PureComponent {
                                     primary
                                     disabled={postDisabled}
                                     onClick={this.props.onPostClick}
-                                    onMouseOver={this.showHint}
-                                    onMouseOut={this.hideHint}
                                 >
                                     {editMode ? tt('post_editor.update') : tt('g.post')}
                                 </Button>
@@ -266,10 +267,16 @@ export default class PostFooter extends PureComponent {
     showPostError(errorText) {
         clearTimeout(this._temporaryErrorTimeout);
 
-        this.setState({ temporaryErrorText: errorText });
+        this.setState({
+            temporaryErrorText: errorText,
+            showHint: true,
+        });
 
         this._temporaryErrorTimeout = setTimeout(() => {
-            this.setState({ temporaryErrorText: null });
+            this.setState({
+                temporaryErrorText: null,
+                showHint: false,
+            });
         }, 5000);
     }
 
