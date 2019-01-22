c73d215c7d16bce00b3eb1173248fa79160710ea
Ilya Lebedev <bacherko@gmail.com>
2019-01-22 16:30:57 +0300

    review fixes #1941

--- server/callServices/meta.js
+++ server/callServices/meta.js
@@ -5,5 +5,7 @@ import { callApi } from './helpers';
 const url = config.get('meta_service_url');
 
 export async function getPostsViewCount(postLinks) {
-    return (await callApi(url, 'getPostsViewCount', { postLinks })).results;
+    const response = await callApi(url, 'getPostsViewCount', { postLinks });
+
+    return response.results;
 }
--- server/callServices/rates.js
+++ server/callServices/rates.js
@@ -5,5 +5,7 @@ import { callApi } from './helpers';
 const url = config.get('rates_service_url');
 
 export async function getActualRates() {
-    return (await callApi(url, 'getActual')).rates;
+    const response = await callApi(url, 'getActual');
+
+    return response.rates;
 }
